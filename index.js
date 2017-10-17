972776890f6381180c1c051c3f0b6f96f9eb8a35
Garrett Cox <garrettjcox@gmail.com>
2017-10-16 19:06:35 -0500

    Update for 1.0.0

--- index.js
+++ index.js
@@ -1,41 +1,46 @@
-'use strict';
 
-const _ = require('lodash');
+const defaultsDeep = require('lodash.defaultsdeep');
 
-module.exports = function() {
+module.exports = function () {
   const server = this;
   const commands = {};
 
-  const config = server.config.command = _.defaultsDeep({}, server.config.command, {
-    prefix: '~'
+  const config = server.config.command = defaultsDeep({}, server.config.command, {
+    prefix: '~',
   });
 
   const regex = new RegExp(`^${config.prefix}([\\w]+)\\s?(.*)`, 'i');
 
-  server.use(require('scriptserver-event'));
+  server
+    .use(require('scriptserver-util'))
+    .use(require('scriptserver-event'));
 
-  server.command = function(cmd, callback) {
+  server.command = (cmd, callback) => {
     cmd = cmd.toLowerCase();
     commands[cmd] = commands[cmd] || [];
     commands[cmd].push(callback);
-  }
+  };
 
-  server.on('chat', event => {
+  server.on('chat', (event) => {
     const stripped = event.message.match(regex);
     if (stripped) {
       const command = stripped[1].toLowerCase();
       server.emit('command', {
         player: event.player,
-        command: command,
+        command,
         args: stripped[2].split(' '),
-        timestamp: Date.now()
+        timestamp: Date.now(),
       });
     }
   });
 
-  server.on('command', event => {
+  server.on('command', (event) => {
     if (commands.hasOwnProperty(event.command)) {
-      commands[event.command].forEach(callback => callback(event));
+      commands[event.command].forEach((callback) => {
+        Promise.resolve()
+          .then(() => callback(event))
+          .catch(e => server.util.tellRaw(e.message, event.player, { color: 'red' }));
+      });
     }
   });
-}
+};
